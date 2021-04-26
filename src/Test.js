4cb3778adbc3ff3b84804d601663c1783e7596d2
ryugaram <gkfka742@naver.com>
2021-04-26 17:52:54 +0900

    비디오 테스트

--- src/Test.js
+++ src/Test.js
@@ -2,10 +2,15 @@ import React,{useEffect,useState} from 'react';
 import axios from 'axios';
 import './Css/Test.css'
 
+// test
+const ENGLAND='ENGLAND'
+
+
+
 // 어제의 날짜 만들기
 let nowday = new Date();  
 let nowtoday=nowday.getFullYear()+"-"+((nowday.getMonth()+1)>10?(nowday.getMonth()+1):"0"+(nowday.getMonth()+1))+"-"+(nowday.getDate()-1);
-console.log(nowtoday);
+// console.log(nowtoday);
 
 //api 데이터 날짜 함수처리
 // function today(day){
@@ -14,19 +19,21 @@ console.log(nowtoday);
 
 // 사이트 이동 함수
 function href(name){
+    name=name.replace(" ","-")
+    // console.log(name)
     return "https://www.scorebat.com/live-stream/"+name
 }
 
 //스코어 및 하이라이트
-function good(d, s, id) {
-     let js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-       js = d.createElement(s); 
-       js.id = id; 
-       js.src = 'https://www.scorebat.com/embed/embed.js?v=arrv'; 
-       fjs.parentNode.insertBefore(js, fjs);
-    }
-    good(document, 'script', 'scorebat-jssdk');
+// function good(d, s, id) {
+//      let js, fjs = d.getElementsByTagName(s)[0];
+//       if (d.getElementById(id)) return;
+//        js = d.createElement(s); 
+//        js.id = id; 
+//        js.src = 'https://www.scorebat.com/embed/embed.js?v=arrv'; 
+//        fjs.parentNode.insertBefore(js, fjs);
+//     }
+//     good(document, 'script', 'scorebat-jssdk');
 
 
 const Test = () => {
@@ -49,24 +56,29 @@ useEffect(()=>{
             {nowtoday}
         </h1>
         <div className="content">
-            <iframe src="https://www.scorebat.com/embed/"  class="_scorebatEmbeddedPlayer_"></iframe>
+            {/* <iframe src="https://www.scorebat.com/embed/"  class="_scorebatEmbeddedPlayer_"></iframe> */}
             {team.map(soc =>{
-                if((soc.date).includes(nowtoday)){
+                if((soc.date).includes(nowtoday)&&(soc.competition.name).includes(ENGLAND)){
                     return ( 
     <>
         <div className="container">
             <div className="itmes">
                 <div className="homeawaycon">
-                    <a href={href(soc.side1.name)}><h2>홈팀 전적 🔍 </h2></a>
-                    <a href={href(soc.side2.name)}><h2>어웨이팀 전적 🔍</h2></a>
+                    <a href={href(soc.side1.name)}><h2>홈팀 정보 🔍 </h2></a>
+                    <a href={href(soc.side2.name)}><h2>어웨이팀 정보 🔍</h2></a>
                 </div>
                 <div className="eachflex">
                     <h3 className="eachitems" key={soc.title}>
                             {soc.title} ({soc.competition.name}) 
                     </h3>
+                
                 </div>
-                <img src={soc.thumbnail}/>
-        </div>
+                <a href={soc.url} ><h4> 하이라이트 보기 📺 </h4></a>
+                <img src={soc.thumbnail}></img>
+                {/* <div className="videoflex">
+                    <div className="video" dangerouslySetInnerHTML={{__html:soc.embed}}></div>  
+                </div> */}
+       </div>
        </div>
     </>
    )}
